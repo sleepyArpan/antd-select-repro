0b1acbbf3d845a9c6d9009edff1bbdf9878dd505
Arpan Chattopadhyay <arpanchattopadhyay23@gmail.com>
2021-07-27 13:53:02 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import 'antd/dist/antd.css';
+import { Select } from 'antd';
+
+const { Option } = Select;
+
+function renderOptions(options) {
+  return options.map((current) => (
+    <Option key={current.id} value={current.value}>
+      <span>{current.value}</span>
+    </Option>
+  ));
+}
 
 function App() {
+  const options = [
+    { id: 220, value: 'Bitcoin' },
+    { id: 221, value: 'Ethereum' },
+    { id: 222, value: 'Cardano' },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '100vh', display: 'grid', placeItems: 'center' }}>
+      <Select
+        placeholder="Select a name"
+        aria-label="cryptoName"
+        onSelect={(option) => console.log(option)}
+      >
+        {renderOptions(options)}
+      </Select>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Hello World', () => {
+  expect(1 + 1).toBe(2);
+});
+
+test('Tests whether select is getting rendered', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(
+    screen.getByRole('combobox', { name: /cryptoName/i })
+  ).toBeInTheDocument();
+  const select = screen.getByRole('combobox', { name: /cryptoName/i });
+  fireEvent.click(select);
+  await screen.findByText(/Bitcoin/i);
+  /* screen.debug();
+  userEvent.click(item1);
+  const { findByText } = within(container);
+  expect(await findByText(/Bitcoin/i)).toBeVisible(); */
 });
